64a166cd7554df7ae1398fd796a28ba6e8ea8cb6
EnWei <10646007@ntub.edu.tw>
2021-09-18 12:11:37 +0800

    Init Project

---
+++ app/src/main/java/tw/idv/louisli/notificationcatcher/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.idv.louisli.notificationcatcher
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
